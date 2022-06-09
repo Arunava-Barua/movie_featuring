a2b1abcd0a6637f0bf8603ddb3677e9418ce5701
Arunava-Barua <rahulbarua31@gmail.com>
2022-06-10 00:47:02 +0530

    Initialize all folder structure and basic routing

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Actors from "./Actors/Actors.jsx";
+import MovieInformation from "./MovieInformation/MovieInformation.jsx";
+import Movies from "./Movies/Movies.jsx";
+import NavBar from "./NavBar/NavBar.jsx";
+import Profile from "./Profile/Profile.jsx";
+
+export {Actors, MovieInformation, Movies, NavBar, Profile};
